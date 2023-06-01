93ce039f30fd80afd29af032039fe77a7fd58fde
Om Prakash Mallik <omprakashmallik91@gmail.com>
2023-06-01 09:54:27 +0530

    almost completed

--- script/script.js
+++ script/script.js
@@ -1,15 +1,15 @@
 // add class navbarDark on navbar scroll
-const header = document.querySelector('.navbar');
-console.log(header)
-window.onscroll = function() {
-    const top = window.scrollY;
-    if(top >=100) {
-        header.classList.add('sticky');
-    }
-    else {
-        header.classList.remove('sticky');
-    }
-}
+// const header = document.querySelector('.navbar');
+// console.log(header)
+// window.onscroll = function() {
+//     const top = window.scrollY;
+//     if(top >=100) {
+//         header.classList.add('sticky');
+//     }
+//     else {
+//         header.classList.remove('sticky');
+//     }
+// }
 // collapse navbar after click on small devices
 const navLinks = document.querySelectorAll('.nav-item')
 const menuToggle = document.getElementById('navbarSupportedContent')
