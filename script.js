19f1e42b21bb37db516dcb1e946deb45032c31e3
Om Prakash Mallik <omprakashmallik91@gmail.com>
2023-07-05 18:55:42 +0530

    nav and home updated

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontent = document.getElementsByClassName("tab-content");
 
-function opentab(tabname){
-    for(tablink of tablinks){
-        tablink.classList.remove("active-link");
-    }
-    for(tablink of tabcontent){
-        tablink.classList.remove("active-tab");
-    }
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add("active-tab");
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tablink of tabcontent) {
+    tablink.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
 }
-
 
 //menu
 var sidemenu = document.getElementById("sidemenu");
 
-function openmenu(){
-    sidemenu.style.right = "0";
+function openmenu() {
+  sidemenu.style.right = "0";
 }
 
-function closemenu(){
-    sidemenu.style.right = "-200px";
+function closemenu() {
+  sidemenu.style.right = "-200px";
 }
+
+var typed = new Typed(".text", {
+  strings: ["Front End Developer", "Programmer",],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
