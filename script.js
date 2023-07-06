cc5677f68171a49378876f74014316805e61650e
Om Prakash Mallik <omprakashmallik91@gmail.com>
2023-07-06 18:05:03 +0530

    home page completed with animation

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function closemenu() {
 }
 
 var typed = new Typed(".text", {
-  strings: ["Front End Developer", "Programmer",],
+  strings: ["Front End Developer", "Programmer", "Web Developer"],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
