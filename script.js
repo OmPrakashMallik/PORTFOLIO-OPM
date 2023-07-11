e99b4e3b25325b0036f0d2cd71f92cbd536da5f7
Om Prakash Mallik <omprakashmallik91@gmail.com>
2023-07-12 01:28:35 +0530

    gg

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontent = document.getElementsByClassName("tab-content");
 
-// function opentab(tabname) {
-//   for (tablink of tablinks) {
-//     tablink.classList.remove("active-link");
-//   }
-//   for (tablink of tabcontent) {
-//     tablink.classList.remove("active-tab");
-//   }
-//   event.currentTarget.classList.add("active-link");
-//   document.getElementById(tabname).classList.add("active-tab");
-// }
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tablink of tabcontent) {
+    tablink.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
 
 
 // active
